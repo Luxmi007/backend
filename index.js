2fe404697b55faf14aa5f45789890ba807bbccfe
Luxmi <luxmi5829@gmail.com>
2024-06-30 02:51:14 +0530

    github data

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const gitHubData = {
+    "login": "Luxmi007",
+    "id": 94300957,
+    "node_id": "U_kgDOBZ7rHQ",
+    "avatar_url": "https://avatars.githubusercontent.com/u/94300957?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/Luxmi007",
+    "html_url": "https://github.com/Luxmi007",
+    "followers_url": "https://api.github.com/users/Luxmi007/followers",
+    "following_url": "https://api.github.com/users/Luxmi007/following{/other_user}",
+    "gists_url": "https://api.github.com/users/Luxmi007/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/Luxmi007/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/Luxmi007/subscriptions",
+    "organizations_url": "https://api.github.com/users/Luxmi007/orgs",
+    "repos_url": "https://api.github.com/users/Luxmi007/repos",
+    "events_url": "https://api.github.com/users/Luxmi007/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/Luxmi007/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Luxmi",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 10,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2021-11-14T13:54:28Z",
+    "updated_at": "2024-06-17T18:47:48Z"
+  }
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -15,6 +50,11 @@ app.get('/twitter', (req,res) => {
 app.get('/login', (req,res) => {
     res.send('<h1> Welcome to the login page<h1/>')
 })
+
+app.get('/github',(req, res) => {
+    res.json(gitHubData)
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })

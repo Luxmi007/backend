a5cc3f066cc9fab72c804e33b8f43c6ac8744c70
Luxmi <luxmi5829@gmail.com>
2024-07-01 01:47:07 +0530

    data added

--- index.js
+++ index.js
@@ -4,40 +4,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const gitHubData = {
-    "login": "Luxmi007",
-    "id": 94300957,
-    "node_id": "U_kgDOBZ7rHQ",
-    "avatar_url": "https://avatars.githubusercontent.com/u/94300957?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/Luxmi007",
-    "html_url": "https://github.com/Luxmi007",
-    "followers_url": "https://api.github.com/users/Luxmi007/followers",
-    "following_url": "https://api.github.com/users/Luxmi007/following{/other_user}",
-    "gists_url": "https://api.github.com/users/Luxmi007/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/Luxmi007/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/Luxmi007/subscriptions",
-    "organizations_url": "https://api.github.com/users/Luxmi007/orgs",
-    "repos_url": "https://api.github.com/users/Luxmi007/repos",
-    "events_url": "https://api.github.com/users/Luxmi007/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/Luxmi007/received_events",
-    "type": "User",
-    "site_admin": false,
-    "name": "Luxmi",
-    "company": null,
-    "blog": "",
-    "location": null,
-    "email": null,
-    "hireable": null,
-    "bio": null,
-    "twitter_username": null,
-    "public_repos": 10,
-    "public_gists": 0,
-    "followers": 0,
-    "following": 0,
-    "created_at": "2021-11-14T13:54:28Z",
-    "updated_at": "2024-06-17T18:47:48Z"
-  }
+const data = [{
+    id: 1,
+    name: 'Luxmi',
+    gender: 'female'
+    },
+    {
+      id: 2,
+      name: 'Lia',
+      gender: 'female'
+      },
+      {
+        id: 3,
+        name: 'Liam',
+        gender: 'male'
+        },
+        {
+          id: 4,
+          name: 'Lola',
+          gender: 'female'
+          },
+          {
+            id: 5,
+            name: 'Lakshay',
+            gender: 'male'
+            },
+    
+]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -51,8 +44,8 @@ app.get('/login', (req,res) => {
     res.send('<h1> Welcome to the login page<h1/>')
 })
 
-app.get('/github',(req, res) => {
-    res.json(gitHubData)
+app.get('/api/data',(req, res) => {
+    res.json(data)
 })
 
 app.listen(process.env.PORT, () => {
